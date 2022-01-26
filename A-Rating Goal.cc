2a5e267d27a7e8ed80c48e79703c8307b67d5749
FoxNoi99 <92666641+FoxNoi99@users.noreply.github.com>
2022-01-26 14:36:08 +0900

    [作成]A-Rating Goal.cc
    A-Rating Goal(https://atcoder.jp/contests/abc076/tasks/abc076_a)

---
+++ A-Rating Goal.cc
@@ -0,0 +1,12 @@
+/*
+https://atcoder.jp/contests/abc076/tasks/abc076_a
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int R,G;
+  cin >> R >> G;
+  cout << 2 * G - R << endl;//式(R+G)/2
+}
